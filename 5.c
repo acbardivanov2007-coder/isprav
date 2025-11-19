b9f0215f9fac40c1983b2aa748b59fd68f21f413
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2025-11-19 23:34:06 +0700

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#define strok 5 
+int main()
+{
+    int a,b;///номер стр. счет пробела
+    char bukva;///буква
+    for (a=0;a<strok; a++)
+    {
+        for (b=0;b<strok-a-1;b++)///пробелы
+        {
+            printf(" "); 
+        }
+        for (bukva='A';bukva<='A'+a;bukva++)///буквы по возр
+        {
+        printf("%c",bukva);
+        }
+        for (bukva='A'+a-1;bukva>='A';bukva--)///буквы по убыв.
+        {
+            printf("%c",bukva);
+        }
+        printf("\n");
+    }
+    return 0;
+}
