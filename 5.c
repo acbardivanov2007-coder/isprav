a39d6d2457c7c794e76c444611320424c9ee11e0
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2025-11-20 16:15:12 +0700

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,22 +1,33 @@
-#include<stdio.h>
-#define strok 5 
+#include <stdio.h>
 int main()
 {
-    int a,b;//номер стр. счет пробела
-    char bukva;//буква
-    for (a=0;a<strok; a++)
+    char vvod; // Введенный символ
+    int a, b; // Счетчики для циклов
+    char bukva; // Переменная для букв
+    printf("Введите прописную букву: ");
+    scanf("%c", &vvod);
+    if (vvod < 'A' || vvod > 'Z') {
+        printf("Ошибка: введите прописную букву от A до Z!\n");
+        return 1;
+    }
+    int strok = vvod - 'A' + 1; // Количество строк вычисляем на основе введенного символа
+    // Внешний цикл для строк пирамиды
+    for (a = 0; a < strok; a++)
     {
-        for (b=0;b<strok-a-1;b++)//пробелы
+        // Первый внутренний цикл для пробелов
+        for (b = 0; b < strok - a - 1; b++)
         {
             printf(" "); 
         }
-        for (bukva='A';bukva<='A'+a;bukva++)//буквы по возр
+        // Второй внутренний цикл для букв по возрастанию
+        for (bukva = 'A'; bukva <= 'A' + a; bukva++)
         {
-        printf("%c",bukva);
+            printf("%c", bukva);
         }
-        for (bukva='A'+a-1;bukva>='A';bukva--)//буквы по убыв.
+        // Третий внутренний цикл для букв по убыванию
+        for (bukva = 'A' + a - 1; bukva >= 'A'; bukva--)
         {
-            printf("%c",bukva);
+            printf("%c", bukva);
         }
         printf("\n");
     }
