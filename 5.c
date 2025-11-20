13d6d82db503efba444685fa40443b530d78f138
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2025-11-20 15:52:54 +0700

    Update 5.c

--- 5.c
+++ 5.c
@@ -2,19 +2,19 @@
 #define strok 5 
 int main()
 {
-    int a,b;///номер стр. счет пробела
-    char bukva;///буква
+    int a,b;//номер стр. счет пробела
+    char bukva;//буква
     for (a=0;a<strok; a++)
     {
-        for (b=0;b<strok-a-1;b++)///пробелы
+        for (b=0;b<strok-a-1;b++)//пробелы
         {
             printf(" "); 
         }
-        for (bukva='A';bukva<='A'+a;bukva++)///буквы по возр
+        for (bukva='A';bukva<='A'+a;bukva++)//буквы по возр
         {
         printf("%c",bukva);
         }
-        for (bukva='A'+a-1;bukva>='A';bukva--)///буквы по убыв.
+        for (bukva='A'+a-1;bukva>='A';bukva--)//буквы по убыв.
         {
             printf("%c",bukva);
         }
